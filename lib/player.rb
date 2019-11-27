dabef82fd2c38b7dcdf9392b6b7e3e43cc001c63
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-27 16:00:42 +0000

    Add player class with name attribute

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,10 @@ class Player
 
   attr_reader :name
 
+  
+
   def initialize(name)
     @name = name
+  
   end
 end 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,4 +7,8 @@ describe 'Player' do
   it "should initialise with a given name, and return it to the user when name is called" do
     expect(subject.name).to eq("Andrew")
   end
+
+  it "should initialize with 100 HP, and return it to the user when hp is called" do 
+    expect(subject.hp).to eq(100)
+  end
 end
